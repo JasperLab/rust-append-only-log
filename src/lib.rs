dbafebd27b7cc8d3711dcd3bdaf87fc0e2ee5c2a
Vladimir Ivanov <vlad.v.ivanov@icloud.com>
2022-09-23 16:13:04 -0400

    defined basic log append and read interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,33 @@
+use std::sync::Mutex;
+use thiserror::Error;
+
 #[allow(dead_code)]
 pub struct Log {
-    records: Vec<Record>,
+    records: Mutex<Vec<Record>>,
+}
+
+#[derive(Error, Debug)]
+#[error("{message}")]
+pub struct LogError {
+    message: String,
 }
 
 #[allow(dead_code)]
 pub struct Record {
     value: Vec<u8>,
     offset: u64,
+}
+
+#[allow(unused_variables)]
+impl Log {
+    pub fn append(record: Record) -> Result<u64, LogError> {
+        Ok(0)
+    }
+
+    pub fn read(offset: u64) -> Result<Record, LogError> {
+        Ok(Record {
+            offset: offset,
+            value: vec![],
+        })
+    }
 }
