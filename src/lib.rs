d15d2f125c33f2c8aa7bf87579e98b45391f5dcb
Vladimir Ivanov <vlad.v.ivanov@icloud.com>
2022-09-24 17:16:56 -0400

    basic append implementation

--- src/lib.rs
+++ src/lib.rs
@@ -15,19 +15,43 @@ pub struct LogError {
 #[allow(dead_code)]
 pub struct Record {
     value: Vec<u8>,
-    offset: u64,
+    offset: usize,
 }
 
 #[allow(unused_variables)]
 impl Log {
-    pub fn append(record: Record) -> Result<u64, LogError> {
-        Ok(0)
+    pub fn new() -> Log {
+        Log {
+            records: Mutex::new(vec![]),
+        }
     }
 
-    pub fn read(offset: u64) -> Result<Record, LogError> {
+    pub fn append(&mut self, mut record: Record) -> Result<usize, LogError> {
+        let mut guard = self.records.lock().unwrap();
+        let offset = guard.len();
+        record.offset = offset;
+        guard.push(record);
+        Ok(offset)
+    }
+
+    pub fn read(&mut self, offset: usize) -> Result<Record, LogError> {
         Ok(Record {
             offset: offset,
             value: vec![],
         })
     }
+}
+
+impl Record {
+    pub fn new(value: Vec<u8>) -> Record {
+        let offset = 0;
+        Record { value, offset }
+    }
+}
+
+#[test]
+fn test_append() {
+    let mut log = Log::new();
+    let record = Record::new(vec![127]);
+    assert_eq!(log.append(record).unwrap(), 0)
 }
